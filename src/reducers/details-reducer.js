52a2138ac2bec220987ca5ff5198e19eb2c2f6b4
liamshen10 <liamshen03@gmail.com>
2023-08-19 23:08:56 -0400

    get rid of console logs

--- src/reducers/details-reducer.js
+++ src/reducers/details-reducer.js
@@ -1,38 +1,35 @@
-import { createSlice } from "@reduxjs/toolkit";
-import { fetchDetails, fetchReviews, addReview, getReviewById, deleteReview } from "../services/details-thunks";
+    import { createSlice } from "@reduxjs/toolkit";
+    import { fetchDetails, fetchReviews, addReview, getReviewById, deleteReview } from "../services/details-thunks";
 
-const detailsSlice = createSlice({
-  name: "details",
-  initialState: {
-    details: null,
-    reviews: [],
-    status: "idle",
-    error: null
-  },
-  reducers: {},
-  extraReducers: {
-    [fetchDetails.fulfilled]: (state, { payload }) => {
-      state.details = payload;
+    const detailsSlice = createSlice({
+    name: "details",
+    initialState: {
+        details: null,
+        reviews: [],
+        status: "idle",
+        error: null
     },
-    [fetchReviews.fulfilled]: (state, { payload }) => {
-      state.reviews = payload;
-    },
-    [addReview.fulfilled]: (state, { payload }) => {
-      state.reviews.push(payload);
-    },
-    [getReviewById.fulfilled]: (state, { payload }) => {
-      console.log("getReviewById Payload:", payload);
-      state.reviews.push(payload);
-    },
-    [deleteReview.fulfilled]: (state, { payload }) => {
-        console.log("Payload: ", payload);
-        const updatedReviews = state.reviews.filter(review => review._id !== payload._id);
-        console.log("Updated Reviews: ", updatedReviews);
-        state.reviews = updatedReviews;
-      },
-      
-      
-  }
-});
+    reducers: {},
+    extraReducers: {
+        [fetchDetails.fulfilled]: (state, { payload }) => {
+        state.details = payload;
+        },
+        [fetchReviews.fulfilled]: (state, { payload }) => {
+        state.reviews = payload;
+        },
+        [addReview.fulfilled]: (state, { payload }) => {
+        state.reviews.push(payload);
+        },
+        [getReviewById.fulfilled]: (state, { payload }) => {
+        state.reviews.push(payload);
+        },
+        [deleteReview.fulfilled]: (state, { payload }) => {
+            const updatedReviews = state.reviews.filter(review => review._id !== payload._id);
+            state.reviews = updatedReviews;
+        },
+        
+        
+    }
+    });
 
-export default detailsSlice.reducer;
+    export default detailsSlice.reducer;
